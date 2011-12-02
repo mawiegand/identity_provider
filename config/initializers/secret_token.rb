2212ca238d58f03dc44b707480056ba2df2ce019
pafox <p@trick-fox.de>
2011-12-02 19:43:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdentityProvider::Application.config.secret_token = 'af17a3b1c734fc32340a1923346329654c28b21b79e36cc1380881f509c12f589fce683c450e1959a4c6cd53e3689a36d1edc66d7c0797b219c436ca67180ce8'
