698d2d414653f23d4f18ca4039379dc451c7dad6
Sascha Lange <sascha77@googlemail.com>
2011-12-07 17:04:41 +0100

    installed annotate (newest version from git, due to a bug)

--- app/models/indentity.rb
+++ app/models/indentity.rb
@@ -1,2 +1,15 @@
 class Indentity < ActiveRecord::Base
 end
+# == Schema Information
+#
+# Table name: indentities
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  password   :string(255)
+#  salt       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- test/unit/indentity_test.rb
+++ test/unit/indentity_test.rb
@@ -5,3 +5,16 @@ class IndentityTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+# == Schema Information
+#
+# Table name: indentities
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  password   :string(255)
+#  salt       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
