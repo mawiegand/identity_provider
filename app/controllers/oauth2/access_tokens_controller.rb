7beaee8da84d17b80698af1f700a5e6836d33890
Sascha Lange <sascha77@googlemail.com>
2012-01-07 20:38:22 +0100

    added additional validations for the password flow

--- app/controllers/oauth2/access_tokens_controller.rb
+++ app/controllers/oauth2/access_tokens_controller.rb
@@ -5,14 +5,59 @@ require 'json'
 module Oauth2
 
   class AccessTokensController < ApplicationController
+    skip_before_filter :verify_authenticity_token, :only => [ :create ] # this prevents setting a session key on a post to the access token endpoint
   
     before_filter :authenticate,    :except => [ :create ]
     before_filter :authorize_staff, :except => [ :create ]
-  
+      
     # implementes the POST method endpoint for obtaining an access token
     def create
+      client_id     = 'XYZ'
+      client_secret = 'passwd'
       
-      if false      # on success
+      logger.debug('Request for access-token with params: ' + params.inspect)
+            
+      # check method; only post allowed
+      if (!request.post?)
+        render_endpoint_error :invalid_request, "This endpoint only supports POST. " +
+          "Your request used HTTP #{ request.method }.";
+        return 
+      end
+      
+      # check encoding
+      if (request.content_type() != "application/x-www-form-urlencoded")
+        render_endpoint_error :invalid_request, "The Content-Type of the message must be " +
+          "application/x-www-form-urlencoded but was " +
+          "#{ ActionController::Base.helpers.sanitize(request.content_type().inspect) }."
+        return 
+      end
+      
+      # check grant_type
+      if !params[:grant_type] 
+        render_endpoint_error :invalid_request, "Your request is missing the grant_type.";
+        return 
+      elsif (params[:grant_type].downcase != 'password') 
+        render_endpoint_error :unsupported_grant_type, "This endpoint only supports the following "+
+          "grant types: password. Please use one of those.";
+        return 
+      end
+            
+      # check client
+      if !params[:client_id] || params[:client_id].blank?
+        render_endpoint_error :invalid_request, "Your request is missing the client_id.";
+        return
+      elsif params[:client_id].split('-')[0] != client_id
+        render_endpoint_error :invalid_client, "This client is unknown.";
+        return
+      end        
+        
+      # check scope or set to default
+      
+      # check username
+      
+      # chek password
+      
+      if true      # on success
         body = {
           :access_token => 'myaccesstoken',
           :token_type => 'bearer',
@@ -24,27 +69,13 @@ module Oauth2
         render :status => :ok, :json => JSON.pretty_generate(body)
         headers['Cache-Control'] = 'no-store'
         headers['Pragma'] = 'no-cache'
-      
+        headers['Connection'] = 'close'
+
         # NOTE: add to tests:
         # check cache control no-store
         # check pragma no-cache
         # check Content-Type is application/json;charset=UTF-8   (compare case-insensitve, remove whitespaces)
-     else      #  on failure
-        body = {
-          :error             => 'invalid_request',   # invalid_client, invalid_grant, unauthorized_client, unsupported_grant_type, invalid_scope
-          :error_description => 'description of the error for the client DEVELOPER',
-          :error_uri         => 'http://www.domain.de/link/to/relevant/developer/documentation'
-        }
 
-        render :status => :bad_request, :json => JSON.pretty_generate(body)
-        headers['Cache-Control'] = 'no-store'
-        headers['Pragma'] = 'no-cache'
-        
-        # NOTE: add to tests:
-        # does include an error attribute with a valid error code
-        # check cache control no-store
-        # check pragma no-cache
-        # check Content-Type is application/json;charset=UTF-8 
      end
        
       
@@ -60,6 +91,28 @@ module Oauth2
     def index
       return 'Generate a list of all handed out access tokens and provide functionallity to revoke individual or all tokens.'
     end
+  
+    protected
+      
+      def render_endpoint_error(error_code, error_description = nil, error_uri = nil)
+
+        body = {
+          :error             => error_code,   # invalid_client, invalid_grant, unauthorized_client, unsupported_grant_type, invalid_scope
+          :error_description => error_description,
+          :error_uri         => error_uri
+        }
+
+        render :status => :bad_request, :json => JSON.pretty_generate(body.delete_if { |k,v| v.blank? })
+        headers['Cache-Control'] = 'no-store'
+        headers['Pragma'] = 'no-cache'
+        headers['Connection'] = 'close'
+        
+        # NOTE: add to tests:
+        # does include an error attribute with a valid error code
+        # check cache control no-store
+        # check pragma no-cache
+        # check Content-Type is application/json;charset=UTF-8   
+      end
   
   end
 
