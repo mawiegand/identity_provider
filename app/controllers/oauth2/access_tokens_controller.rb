6b17ebbc3b54fcc788e964465190fcb10853083f
Sascha Lange <sascha77@googlemail.com>
2012-01-07 17:41:40 +0100

    extend acces_token stub to creaing error responses

--- app/controllers/oauth2/access_tokens_controller.rb
+++ app/controllers/oauth2/access_tokens_controller.rb
@@ -11,19 +11,42 @@ module Oauth2
   
     # implementes the POST method endpoint for obtaining an access token
     def create
+      
+      if false      # on success
+        body = {
+          :access_token => 'myaccesstoken',
+          :token_type => 'bearer',
+          :expires_in => 3600,
+          :refresh_token => 'myrefreshtoken',
+          :additional_parameter => 'my_example_value'
+        }
 
-      body = {
-        :access_token => 'myaccesstoken',
-        :token_type => 'bearer',
-        :expires_in => 3600,
-        :refresh_token => 'myrefreshtoken',
-        :additional_parameter => 'my_example_value'
-      }
+        render :status => :ok, :json => JSON.pretty_generate(body)
+        headers['Cache-Control'] = 'no-store'
+        headers['Pragma'] = 'no-cache'
+      
+        # NOTE: add to tests:
+        # check cache control no-store
+        # check pragma no-cache
+        # check Content-Type is application/json;charset=UTF-8   (compare case-insensitve, remove whitespaces)
+     else      #  on failure
+        body = {
+          :error             => 'invalid_request',   # invalid_client, invalid_grant, unauthorized_client, unsupported_grant_type, invalid_scope
+          :error_description => 'description of the error for the client DEVELOPER',
+          :error_uri         => 'http://www.domain.de/link/to/relevant/developer/documentation'
+        }
 
-      render :status => :ok, :json => JSON.pretty_generate(body)
-  #    headers['Content-Type'] = 'application/x-www-form-urlencoded'
-      headers['Cache-Control'] = 'no-store'
-      headers['Pragma'] = 'no-cache'
+        render :status => :bad_request, :json => JSON.pretty_generate(body)
+        headers['Cache-Control'] = 'no-store'
+        headers['Pragma'] = 'no-cache'
+        
+        # NOTE: add to tests:
+        # does include an error attribute with a valid error code
+        # check cache control no-store
+        # check pragma no-cache
+        # check Content-Type is application/json;charset=UTF-8 
+     end
+       
       
     end
     
