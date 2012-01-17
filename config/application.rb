abc4db14129733dfd835704a6a58345ebdd20831
Patrick Fox <p@trick-fox.de>
2012-01-17 23:28:14 +0100

    constrain ssl to production environment

--- config/application.rb
+++ config/application.rb
@@ -44,8 +44,5 @@ module IdentityProvider
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
-    # Use ssl
-    config.force_ssl = true
   end
 end
