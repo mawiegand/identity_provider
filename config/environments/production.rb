93bbf6420ca779d4d52d11a6c9f4fb013ceba7a0
Patrick Fox <p@trick-fox.de>
2012-01-17 23:41:55 +0100

    added custom assets to precompile-array

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ IdentityProvider::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['client.css', 'identities.css', 'log_entries.css', 'scaffold.css', 'sessions.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
