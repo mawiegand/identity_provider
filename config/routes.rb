7ae9488556202f736406917e625f8617926f359b
Patrick Fox <p@trick-fox.de>
2012-01-19 14:22:45 +0100

    working on moving to subdirectory

--- config/routes.rb
+++ config/routes.rb
@@ -13,30 +13,31 @@ IdentityProvider::Application.routes.draw do
 
   # all resources and paths are scoped in an optional path_prefix determining the
   # locale to use. Presently only available: en, de
-  scope "(:locale)", :locale => /en|de/ do   
-    match '/identities/self', :to => 'identities#self'
-
-    
-    resources :identities,  :only => [:new, :create, :show, :index, :edit, :destroy, :update]
-    resources :sessions,    :only => [:new, :create, :destroy]
-    resources :log_entries, :only => [:index]
-    resources :clients
-    
-    
-    namespace :oauth2 do
-      match :access_token, :to => 'access_tokens#create'
-      resources :access_tokens, :only => [ :index, :show, :delete ] # routes for administering issued access tokens
+  scope "/identity_provider" do
+    scope "(:locale)", :locale => /en|de/ do   
+      match '/identities/self', :to => 'identities#self'
+  
+      
+      resources :identities,  :only => [:new, :create, :show, :index, :edit, :destroy, :update]
+      resources :sessions,    :only => [:new, :create, :destroy]
+      resources :log_entries, :only => [:index]
+      resources :clients
+      
+      
+      namespace :oauth2 do
+        match :access_token, :to => 'access_tokens#create'
+        resources :access_tokens, :only => [ :index, :show, :delete ] # routes for administering issued access tokens
+      end
+          
+      match '/signin',  :to => 'sessions#new'
+      match '/signout', :to => 'sessions#destroy'
+  
+      match '/identities/:id/validation', :to => 'identities#validation'
     end
-        
-    match '/signin',  :to => 'sessions#new'
-    match '/signout', :to => 'sessions#destroy'
-
-    match '/identities/:id/validation', :to => 'identities#validation'
-  end
    
-  match '/:locale' => 'sessions#new'         # match e.g. /de/
-  root :to => 'sessions#new'                 # redirect to signin
-
+    match '/:locale' => 'sessions#new'         # match e.g. /de/
+    root :to => 'sessions#new'                 # redirect to signin
+  end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
