c5ef26a7e57e707be505a4f3d612c7bf1b4b71c2
Your Name <p@trick-fox.de>
2012-07-19 17:51:42 +0200

    drop down fields for singupand signin mode

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,18 +3,18 @@ class Client < ActiveRecord::Base
   has_many  :grants,  :class_name => "GrantedScope", :foreign_key => :client_id, :inverse_of => :client
   
   SIGNUP_MODES = []
+  SIGNUP_MODE_OFF = 0
+  SIGNUP_MODES[SIGNUP_MODE_OFF] = :off
   SIGNUP_MODE_NORMAL = 1
   SIGNUP_MODES[SIGNUP_MODE_NORMAL] = :normal
-  SIGNUP_MODE_OFF = 2
-  SIGNUP_MODES[SIGNUP_MODE_OFF] = :off
-  SIGNUP_MODE_INVITATION = 3  
+  SIGNUP_MODE_INVITATION = 2  
   SIGNUP_MODES[SIGNUP_MODE_INVITATION] = :invitation
   
   SIGNIN_MODES = []
+  SIGNIN_MODE_OFF = 0
+  SIGNIN_MODES[SIGNIN_MODE_OFF] = :off
   SIGNIN_MODE_ON = 1
   SIGNIN_MODES[SIGNIN_MODE_ON] = :on
-  SIGNIN_MODE_OFF = 2
-  SIGNIN_MODES[SIGNIN_MODE_OFF] = :off
   
   def scope_authorized?(scope)
     return scopes.split(' ').include?(scope.downcase())
