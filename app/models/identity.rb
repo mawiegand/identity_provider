254be412c305beea35619032809ce70b35cf0c76
Sascha Lange <sascha77@googlemail.com>
2012-07-24 19:20:16 +0200

    wrong foreing key for resources in identity

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -57,9 +57,9 @@ class Identity < ActiveRecord::Base
   has_many  :log_entries;
   has_many  :grants,  :class_name => "GrantedScope", :foreign_key => :identity_id, :inverse_of => :identity
   
-  has_many  :results,               :class_name => "Resource::Result",            :foreign_key => :game_id, :inverse_of => :identity
-  has_many  :events,                :class_name => "Resource::History",           :foreign_key => :game_id, :inverse_of => :identity
-  has_many  :character_properties,  :class_name => "Resource::CharacterProperty", :foreign_key => :game_id, :inverse_of => :identity
+  has_many  :results,               :class_name => "Resource::Result",            :foreign_key => :identity_id, :inverse_of => :identity
+  has_many  :events,                :class_name => "Resource::History",           :foreign_key => :identity_id, :inverse_of => :identity
+  has_many  :character_properties,  :class_name => "Resource::CharacterProperty", :foreign_key => :identity_id, :inverse_of => :identity
   
   
   attr_accessor :password
