1a72a77c6f0e01adb58e13917b755e3bf16a4c65
Sascha Lange <sascha77@googlemail.com>
2012-07-24 14:31:29 +0200

    scaffolded character properties

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 IdentityProvider::Application.routes.draw do
 
+  namespace :resource do resources :character_properties end
+
   namespace :resource do resources :results end
 
   namespace :resource do resources :histories end
