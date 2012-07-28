0ba9628601281b01a1364d80ed328055bbea4ad8
Your Name <p@trick-fox.de>
2012-07-28 11:35:56 +0200

    localization

--- app/mailers/identity_mailer.rb
+++ app/mailers/identity_mailer.rb
@@ -5,7 +5,7 @@ class IdentityMailer < ActionMailer::Base
     @identity = identity
     @validation_url = root_url(:host => 'wack-a-doo.de')  + "/identities/#{identity.id}/validation?code=#{identity.validation_code}"
     
-    mail :to => identity.email, :subject => "Welcome to Wack-a-Doo."
+    mail :to => identity.email, :subject => I18n.t('mailing.validation.subject') 
   end
   
   def waiting_list_email(identity, invitation=nil)
@@ -13,21 +13,23 @@ class IdentityMailer < ActionMailer::Base
     @invitation = invitation
     @validation_url = root_url(:host => 'wack-a-doo.de')  + "/identities/#{identity.id}/validation?code=#{identity.validation_code}"
     
-    mail :to => identity.email, :subject => "You're on the waiting list to Wack-a-Doo."    
+    mail :to => identity.email, :subject => I18n.t('mailing.waiting_list.subject')   
   end
   
+
   def password_token_email(identity)
+    logger.debug locale.inspect
     @identity           = identity
-    @password_token_url = IDENTITY_PROVIDER_CONFIG['portal_base_url'] + "/new_password/#{identity.id}/#{identity.password_token}"
+    @password_token_url = IDENTITY_PROVIDER_CONFIG['portal_base_url'] + '/' + I18n.locale.to_s + "/new_password/#{identity.id}/#{identity.password_token}"
     
-    mail :to => identity.email, :subject => "Your Password Link"    
+    mail :to => identity.email, :subject => I18n.t('mailing.password_token.subject')    
   end
   
   def password_email(identity, password)
     @identity = identity
     @password = password
     
-    mail :to => identity.email, :subject => "Your new Password"    
+    mail :to => identity.email, :subject => I18n.t('mailing.password.subject')
   end
   
 end
