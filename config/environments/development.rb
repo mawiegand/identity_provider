beafa72839f797cf0a523e38b510d580edde363b
Patrick <p@trick-fox.de>
2012-09-29 20:23:51 +0200

    changed back to identity provider in subfolder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,5 @@ IdentityProvider::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.assets.prefix = "/assets"
+  config.assets.prefix = "/identity_provider/assets"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,53 +3,55 @@ IdentityProvider::Application.routes.draw do
   # all resources and paths are scoped in an optional path_prefix determining the
   # locale to use. Presently only available: en, de
 
-  scope "(:locale)", :locale => /en|de/ do   
-    match '/identities/self', :to => 'identities#self'
-    
-    resource  :dashboard,             :controller => "Dashboard", :only => [:show, :create]
-    
-    resources :identities,  :only => [:new, :create, :show, :index, :edit, :destroy, :update] do
-      resources :messages 
+  scope "/identity_provider" do
+    scope "(:locale)", :locale => /en|de/ do   
+      match '/identities/self', :to => 'identities#self'
+      
+      resource  :dashboard,             :controller => "Dashboard", :only => [:show, :create]
+      
+      resources :identities,  :only => [:new, :create, :show, :index, :edit, :destroy, :update] do
+        resources :messages 
+      end
+      resources :sessions,    :only => [:new, :create, :destroy]
+      resources :log_entries, :only => [:index]
+      resources :clients
+      resources :granted_scopes
+      resources :keys
+      
+      resources :character_properties,  :path => "identities/:identity_id/character_properties",  :module => 'resource'            
+      resources :results,               :path => "identities/:identity_id/results",               :module => 'resource'            
+      resources :histories,             :path => "identities/:identity_id/histories",             :module => 'resource'            
+  
+      resources :messages
+  
+  
+      namespace :resource do 
+        resources :character_properties
+        resources :results
+        resources :histories
+        resources :games 
+      end
+      
+      namespace :oauth2 do
+        match :access_token, :to => 'access_tokens#create'
+        match :redirect_test_start, :to => 'access_tokens#redirect_test_start'
+        match :redirect_test_end,   :to => 'access_tokens#redirect_test_end'
+        resources :access_tokens,   :only => [ :index, :show, :delete ] # routes for administering issued access tokens
+      end
+          
+      match '/signin',  :to => 'sessions#new'
+      match '/signout', :to => 'sessions#destroy'
+  
+      match '/identities/:id/validation', :to => 'identities#validation'
+      match '/identities/:id/signin',     :to => 'identities#signin'
+  
+      match '/send_password_token', :to => 'identities#send_password_token'
+      match '/send_password',       :to => 'identities#send_password'
     end
-    resources :sessions,    :only => [:new, :create, :destroy]
-    resources :log_entries, :only => [:index]
-    resources :clients
-    resources :granted_scopes
-    resources :keys
-    
-    resources :character_properties,  :path => "identities/:identity_id/character_properties",  :module => 'resource'            
-    resources :results,               :path => "identities/:identity_id/results",               :module => 'resource'            
-    resources :histories,             :path => "identities/:identity_id/histories",             :module => 'resource'            
-
-    resources :messages
-
-
-    namespace :resource do 
-      resources :character_properties
-      resources :results
-      resources :histories
-      resources :games 
-    end
-    
-    namespace :oauth2 do
-      match :access_token, :to => 'access_tokens#create'
-      match :redirect_test_start, :to => 'access_tokens#redirect_test_start'
-      match :redirect_test_end,   :to => 'access_tokens#redirect_test_end'
-      resources :access_tokens,   :only => [ :index, :show, :delete ] # routes for administering issued access tokens
-    end
-        
-    match '/signin',  :to => 'sessions#new'
-    match '/signout', :to => 'sessions#destroy'
-
-    match '/identities/:id/validation', :to => 'identities#validation'
-    match '/identities/:id/signin',     :to => 'identities#signin'
-
-    match '/send_password_token', :to => 'identities#send_password_token'
-    match '/send_password',       :to => 'identities#send_password'
+   
+    match '/:locale' => 'sessions#new'         # match e.g. /de/
+    root :to => 'sessions#new'                 # redirect to signin
   end
- 
-  match '/:locale' => 'sessions#new'         # match e.g. /de/
-  root :to => 'sessions#new'                 # redirect to signin
 
   # See how all your routes lay out with "rake routes"
 end
