5961e1373069de070add2eb2ea805e3f5d449412
salange <sascha@5dlab.com>
2012-12-20 19:05:59 +0100

    don't deliver emails to generic mails

--- app/mailers/identity_mailer.rb
+++ app/mailers/identity_mailer.rb
@@ -1,80 +1,67 @@
 class IdentityMailer < ActionMailer::Base
   default from: "team@5dlab.com"
+  
+  def do_not_deliver!
+    Rails.logger.debug "Stopped delivery of email."
+    def self.deliver! ; false ; end
+  end
+    
 
   def manually_granted_access_email(identity, client)
+    do_not_deliver! if identity.generic_email? 
+
     @identity = identity
     @client = client
     @link = request.protocol + request.host
-    
-    if @identity.generic_email? 
-      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
-      return
-    end
     
     mail :to => identity.email, :subject => I18n.t('mailing.manually_granted_access.subject') 
   end
   
   def automatically_granted_access_email(identity, client)
+    do_not_deliver! if identity.generic_email? 
+
     @identity = identity
     @client = client
     @link = request.protocol + request.host
 
-    if @identity.generic_email? 
-      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
-      return
-    end    
-    
     mail :to => identity.email, :subject => I18n.t('mailing.automatically_granted_access.subject') 
   end
   
   def validation_email(identity)
+    do_not_deliver! if identity.generic_email? 
+
     @identity = identity
     @link = request.protocol + request.host
     @validation_url = @link + "/identity_provider/identities/#{identity.id}/validation?code=#{identity.validation_code}"
-    
-    if @identity.generic_email? 
-      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
-      return
-    end    
     
     mail :to => identity.email, :subject => I18n.t('mailing.validation.subject') 
   end
   
   def waiting_list_email(identity, client, invitation=nil)
+    do_not_deliver! if identity.generic_email? 
+
     @identity   = identity
     @client = client
     @link = request.protocol + request.host
-    
-    if @identity.generic_email? 
-      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
-      return
-    end    
     
     mail :to => identity.email, :subject => I18n.t('mailing.waiting_list.subject')   
   end
   
 
   def password_token_email(identity)
-    logger.debug locale.inspect
+    do_not_deliver! if identity.generic_email? 
+
     @identity           = identity
     @password_token_url = IDENTITY_PROVIDER_CONFIG['portal_base_url'] + '/' + I18n.locale.to_s + "/new_password/#{identity.id}/#{identity.password_token}"
-    
-    if @identity.generic_email? 
-      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
-      return
-    end    
     
     mail :to => identity.email, :subject => I18n.t('mailing.password_token.subject')    
   end
   
   def password_email(identity, password)
+    do_not_deliver! if identity.generic_email? 
+
     @identity = identity
     @password = password
-    
-    if @identity.generic_email? 
-      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
-      return
-    end    
     
     mail :to => identity.email, :subject => I18n.t('mailing.password.subject')
   end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,12 +1,13 @@
 class MessageMailer < ActionMailer::Base
   default from: "chef@wack-a-doo.de"
+
+  def do_not_deliver!
+    Rails.logger.debug "Stopped delivery of email."
+    def self.deliver! ; false ; end
+  end  
   
   def message_email(identity, message)
-
-    if identity.generic_email? 
-      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
-      return
-    end    
+    do_not_deliver! if identity.generic_email? 
     
     mail :to => identity.email, :subject => message.subject do |format|
       format.text { render :text => message.body }
