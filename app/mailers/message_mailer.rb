00f918a9a9004156fe61c417de603728468dbaf5
salange <sascha@5dlab.com>
2012-12-20 18:56:50 +0100

    dont send messages to generic emails

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,6 +2,12 @@ class MessageMailer < ActionMailer::Base
   default from: "chef@wack-a-doo.de"
   
   def message_email(identity, message)
+
+    if identity.generic_email? 
+      Rails.logger.debug "Could not send email to #{ identity.identifier } because this identity has a generic email: #{ identity.email }."
+      return
+    end    
+    
     mail :to => identity.email, :subject => message.subject do |format|
       format.text { render :text => message.body }
       format.html { render :text => message.body.gsub(/\n/,'<br />') }
