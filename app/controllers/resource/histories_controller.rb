dc1d87c0301edf6b68ee27c26348048cb6614c53
Patrick <p@trick-fox.de>
2013-02-28 17:41:09 +0100

    removed hack concerning an old rails version

--- app/controllers/resource/histories_controller.rb
+++ app/controllers/resource/histories_controller.rb
@@ -22,15 +22,7 @@ class Resource::HistoriesController < ApplicationController
       else
         @histories = identity.events.where(:game_id => current_game.id)
       end
-
-      if Rails.env.development?
-        # eval the serialized attributes. otherwise, the quotation marks would be escaped in json output string
-        @histories.each do |event|
-          event.data = eval(event.data)
-          event.localized_description = eval(event.localized_description)        
-        end        
-      end
-    else 
+    else
       @asked_for_index = true
     end
 
