7eb2e8526d3dd390dad5bf1ff55d6f3d632268c8
Patrick <p@trick-fox.de>
2013-03-14 19:41:53 +0100

    prepared for multi stage deployment

--- Gemfile
+++ Gemfile
@@ -34,14 +34,8 @@ end
 gem 'faker'
 gem 'jquery-rails'
 
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# Use unicorn as the web server
-# gem 'unicorn'
-
 # Deploy with Capistrano
-gem 'capistrano'
+gem 'capistrano', '~> 2.12.0'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,27 @@
 require "bundler/capistrano"
 
+set :stages, %w(production staging_test)
+set :default_stage, "production"
+
+require "capistrano/ext/multistage"
+
 `ssh-add`
 
 default_run_options[:pty] = true                  # problem with ubuntu
 set :ssh_options, :forward_agent => true          # ssh forwarding
-set :gateway, 'wackadoo.de:5775'
-#set :port, 5775
+#set :gateway, 'wackadoo.de:5775'
+set :port, 5775
 
 set :application, "identity provider"
 set :repository,  "git@github.com:wackadoo/identity_provider.git"
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :user, "deploy-ip"
 set :use_sudo, false
 
 set :deploy_to, "/var/www/identity_provider"
 set :deploy_via, :remote_cache
-
-role :web, "portal.wack-a-doo.de", "test1.wack-a-doo.de"   # Your HTTP server, Apache/etc
-role :app, "portal.wack-a-doo.de", "test1.wack-a-doo.de"   # This may be the same as your `Web` server
-role :db,  "portal.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
 
 namespace :deploy do
   desc "Restart Thin"
@@ -45,7 +32,7 @@ namespace :deploy do
 
   desc "Reset DB"
   task :reset do
-    run "cd #{current_path}; bundle exec rake RAILS_ENV=\"production\" db:reset"
+    run "cd #{current_path}; bundle exec rake RAILS_ENV=\"#{stage}\" db:reset"
     restart
   end
 
