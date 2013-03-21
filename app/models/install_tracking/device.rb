a563fff768aaf9c98c300a5e3761b7044ad28b46
salange <sascha@5dlab.com>
2013-03-21 17:27:21 +0100

    more information in profile, bug fix in relation

--- app/models/install_tracking/device.rb
+++ app/models/install_tracking/device.rb
@@ -1,7 +1,7 @@
 class InstallTracking::Device < ActiveRecord::Base
   
   has_many  :device_users,  :class_name => "InstallTracking::DeviceUser", :foreign_key => :device_id,  :inverse_of => :device
-  has_many  :installs,      :class_name => "InstallTracking::Install",    :foreign_key => :install_id, :inverse_of => :device
+  has_many  :installs,      :class_name => "InstallTracking::Install",    :foreign_key => :device_id,  :inverse_of => :device
   has_many  :identities,    :through    => :device_users,                                              :inverse_of => :devices
 
   scope     :hardware,         lambda { |string| where(['hardware_string = ?',  string]) }
