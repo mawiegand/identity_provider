a97c023a3b4ddcedd1078f79fad0c63d1a973660
salange <sascha@5dlab.com>
2013-03-26 20:31:35 +0100

    bulk email script

--- script/bulk_mail.rb
+++ script/bulk_mail.rb
@@ -9,21 +9,27 @@ puts "Start sending bulk emails"
 
 count = 0
 
+file = File.new(File.join(Rails.root, "sent_emails.txt"), 'w' );
+
 #Identity.where(["(banned IS NULL OR banned = ?) AND email NOT LIKE '%deleted%' AND email NOT LIKE '%5dlab.com' AND email NOT LIKE '%pfox.eu' AND email NOT LIKE 'hajo%web.de'", false]).all do |identity|
-Identity.where("email LIKE '%pfox.eu'").all do |identity|
+Identity.where("email LIKE '%pfox.eu'").each do |identity|
   
   IdentityMailer.all_players_notice_email(identity, "Soeben ist Runde 3 von Wack-A-Doo gestartet!").deliver
   count = count + 1
   
-  if count % 10 == 0
+  file.write("#{identity.email},\n")
+  puts "#{identity.email}"
+  
+  if count % 5 == 0
     sleep 5
   end
   
-  if count % 5 == 0
-    puts "."
+  if count % 25 == 0
+    puts "\n--------------- #{count}\n"
   end
   
 end
 
+file.close
 
-puts "NPC PLACEMENT: Finished all tasks."
+puts "Sent all emails."
