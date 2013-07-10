1a63d8403913810d1c9ca408ad66080966773bee
Patrick <p@trick-fox.de>
2013-07-10 12:16:56 +0200

    fix: correct url

--- app/controllers/shop/callback_controller.rb
+++ app/controllers/shop/callback_controller.rb
@@ -3,7 +3,7 @@ require 'httparty'
 class Shop::CallbackController < ApplicationController
 
   def redirect
-    response = HTTParty.post('https://localhost/game_server/shop/special_offers_transactions/new',
+    response = HTTParty.post('https://test1.wack-a-doo.de/game_server/shop/special_offers_transactions/new',
                              :body => {:offerID => '4711', :partnerUserID => 'OGvYwFVEMESYXCLT'},
                              :headers => { 'Accept' => 'application/json'})
 
