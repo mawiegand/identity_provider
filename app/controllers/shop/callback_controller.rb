e141d333d68bc98c00788374beb46339b1dbfb8a
Patrick <p@trick-fox.de>
2013-07-10 13:25:59 +0200

    finished shop callback proxy for game server

--- app/controllers/shop/callback_controller.rb
+++ app/controllers/shop/callback_controller.rb
@@ -3,11 +3,26 @@ require 'httparty'
 class Shop::CallbackController < ApplicationController
 
   def redirect
-    response = HTTParty.post('https://test1.wack-a-doo.de/game_server/shop/special_offers_transactions/new',
-                             :body => {:offerID => '4711', :partnerUserID => 'OGvYwFVEMESYXCLT'},
+
+    logger.debug "POST PARAMS " + request.request_parameters.inspect
+
+    # redirect callbacks for offerID 761 (special offer) to production game server
+    if !params[:offerID].nil? && params[:offerID] == '761'
+
+      response = HTTParty.post('https://localhost/game_server/shop/special_offers_transactions',
+                             :body => request.request_parameters,
                              :headers => { 'Accept' => 'application/json'})
 
-    render json: {:status => 'created'}, status: :created
+      if response.code == 201
+        # send 201 for successful callback on
+        render json: {:status => 'special offer processed'}, status: :created
+      else
+        # if not successful, send response code and message provided by game server
+        render json: {:status => response.message}, status: response.code
+      end
+    else
+      # send 201 for unknown offerIDs to prevent continuous retrying of callbacks
+      render json: {:status => 'unknown offerID'}, status: :created
+    end
   end
-
 end
