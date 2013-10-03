9e13fa75fc7b38dc4e381a29c093b0319e047f4c
Patrick <p@trick-fox.de>
2013-10-03 17:10:42 +0200

    unterstand to use regexp the right way...

--- script/push_notification/send_push_notification.rb
+++ script/push_notification/send_push_notification.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
+#
+# script for sending mass push notifications for all registered identities
+#
+# TODO: don't send each nitification separately
+
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment'))
 
@@ -18,9 +23,9 @@ text = 'Runde 4 soeben gestartet: Sei jetzt von Anfang an dabei!'
 Identity.all.each do |identity|
   push_token = identity.push_tokens.order('updated_at').last
   unless push_token.nil?
-    parsed_token = push_token.push_notification_token.sub(/</, '').sub(/>/, '').sub(/ /, '').sub(/ /, '').sub(/ /, '').sub(/ /, '').sub(/ /, '').sub(/ /, '').sub(/ /, '')
+    parsed_token = push_token.push_notification_token.sub(/[<> ]/, '') # parse token
     puts "send token to #{identity.nickname} #{identity.email} #{parsed_token}"
-    #APNS.send_notification(parsed_token, text)
+    APNS.send_notification(parsed_token, text)
     sleep 1
   end
 end
