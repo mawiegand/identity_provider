0109d3697e157c83c1b38f0a7a976b83ec57905d
Sascha Lange <sascha@5dlab.com>
2014-11-17 17:23:04 +0100

    store ref_id and sub_id

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -156,6 +156,8 @@ class IdentitiesController < ApplicationController
           identity.email = email
           identity.locale = I18n.locale
           identity.referer = referer.blank? ? "none" : referer[0..250]
+          identity.ref_id = params[:refid] || params[:ref_id] || nil
+          identity.sub_id = params[:subid] || params[:sub_id] || nil
           identity.password = params[:password]
           identity.password_confirmation = params[:password_confirmation]
           identity.generic_nickname = params[:nickname].blank?
