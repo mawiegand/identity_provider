c273addce995a01235af2b2ea619da6db927bfdf
Sascha Lange <sascha@5dlab.com>
2014-11-17 18:55:34 +0100

    use advertiser token for attribution

--- script/create_random_ref_ids.rb
+++ script/create_random_ref_ids.rb
@@ -12,15 +12,15 @@ base = 'https://wack-a-doo.de/identity_provider'
 refs = [ "test_1", "test_2", "test_3", "test_4", "test_5", "test_6", "test_7", "test_7", "test_8", "test_9"  ]
 subs = [ "01", "02", "03", "03", "03", "04", "05", "05"]
 
-devices = InstallTracking::Device.select('DISTINCT vendor_token').limit(2)
+devices = InstallTracking::Device.select('DISTINCT advertiser_token').limit(2)
 
 devices.each do |device|
-  unless device[:vendor_token].blank?
-    token = device[:vendor_token]
+  unless device[:advertiser_token].blank?
+    token = device[:advertiser_token]
     ref = refs.sample
     sub = subs.sample
 
-    url  = base + "/trac?ref_id=" + ref + "&sub_id=" + sub + "&device_id=" + token
+    url  = base + "/track?ref_id=" + ref + "&sub_id=" + sub + "&device_id=" + token
     HTTParty.get(url)
   end
 end
