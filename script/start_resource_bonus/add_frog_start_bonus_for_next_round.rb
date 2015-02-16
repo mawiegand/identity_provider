c7a8d8ab9b104c9ea0cdeeba5e1dc8dca985caaf
salange <sascha@5dlab.com>
2015-02-16 19:12:24 +0100

    corrections

--- script/start_resource_bonus/add_frog_start_bonus_for_next_round.rb
+++ script/start_resource_bonus/add_frog_start_bonus_for_next_round.rb
@@ -23,7 +23,9 @@ puts "Adding #{frog_bonus_amount} frogs to all identities which signed in during
 identities = Identity.joins("LEFT OUTER JOIN log_entries ON log_entries.identity_id = identities.id")
              .where("log_entries.identity_id IS NOT NULL AND (event_type ='signin_success' OR event_type = 'auth_token_success')")
              .where(["log_entries.created_at > ?", last_round_start_date-1.days])
-             .select("DISTINCT(identity.id)")
+             .select("DISTINCT(identity.id), identities.email")
+             
+puts "Found #{identities.count} identities."
 
 # PRODUCES THE FOLLOWING SQL:
 # SELECT DISTINCT(identity.id) FROM "identities" LEFT OUTER JOIN log_entries ON log_entries.identity_id = identities.id WHERE (log_entries.identity_id IS NOT NULL AND (event_type ='signin_success' OR event_type = 'auth_token_success')) AND (log_entries.created_at > '2014-12-18 17:59:08')
@@ -31,14 +33,13 @@ identities = Identity.joins("LEFT OUTER JOIN log_entries ON log_entries.identity
 
 # for each identity
 identities.each do |identity|
-  puts "Identity: #{identity.id}"
+  puts "Identity: #{identity.id}, #{identity.email}"
     
     # add frog bonus amount
    #  Resource::CharacterProperty.create(game_id: 1,
    #                                     identity_id: identity.id,
   #                                     data: {client_identifier: "wackadoo", start_resource_bonus: [{resource_type_id: 3, amount: frog_bonus_amount}]}
   #  )
-  end
 end
 
 puts "Finished."
